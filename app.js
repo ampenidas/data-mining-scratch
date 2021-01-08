83f29e4bca03cac9c033f851da92bd93b74c1562
Alper Aktas <alperaktas28@gmail.com>
2021-01-08 23:49:38 +0300

    resourceType blocking added

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const express = require('express');
 const puppeteer = require ('puppeteer');
 const app = express();
 const port = 3000;
+const blockResources = ['stylesheet', 'script', 'font', 'media'];
 
 app.get('/', (req, res) => {
     puppeteer.launch({headless: true})
         .then(async browser => {
             console.time('start');
             const page = await browser.newPage ();
-            await page.goto ('https://www.goodreads.com/choiceawards/best-fiction-books-2020');
-            const body = await page.evaluate(() => document.querySelector());
+            await page.setRequestInterception(true);
+            page.on('request', request => {
+                if (blockResources.includes(request.resourceType())) {
+                    request.abort();
+                }
+                else {
+                    request.continue();
+                }
+            });
+            await page.goto ('');
+            const body = await page.evaluate(() => document.body.innerHTML);
             await browser.close();
             console.timeEnd('start');
             res.send(body);
