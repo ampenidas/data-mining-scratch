77e36b7942620386288b2f5d4e151b9f4741bd32
Alper Aktas <alperaktas28@gmail.com>
2021-01-09 17:28:01 +0300

    plugins added

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require('express');
-const puppeteer = require ('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const stealthPlugin = require('puppeteer-extra-plugin-stealth')();
+const adBlockerPlugin = require('puppeteer-extra-plugin-adblocker');
+const blockResourcesPlugin = require('puppeteer-extra-plugin-block-resources')();
+const anonymizeuaPlugin = require('puppeteer-extra-plugin-anonymize-ua')();
+
 const app = express();
 const port = 3000;
-const blockResources = ['stylesheet', 'script', 'font', 'media'];
+const url = '';
+
+
+puppeteer.use(stealthPlugin);
+//puppeteer.use(adBlockerPlugin({blockTrackers: true}));
+puppeteer.use(blockResourcesPlugin);
+puppeteer.use(anonymizeuaPlugin);
 
 app.get('/', (req, res) => {
     puppeteer.launch({headless: true})
         .then(async browser => {
             console.time('start');
-            const page = await browser.newPage ();
-            await page.setRequestInterception(true);
-            page.on('request', request => {
-                if (blockResources.includes(request.resourceType())) {
-                    request.abort();
-                }
-                else {
-                    request.continue();
-                }
-            });
-            await page.goto ('');
+            const page = await browser.newPage();
+
+            blockResourcesPlugin.blockedTypes.add('image');
+            blockResourcesPlugin.blockedTypes.add('stylesheet');
+            blockResourcesPlugin.blockedTypes.add('other');
+            blockResourcesPlugin.blockedTypes.add('script');
+            blockResourcesPlugin.blockedTypes.add('media');
+            blockResourcesPlugin.blockedTypes.add('font');
+
+            await page.goto(url);
             const body = await page.evaluate(() => document.body.innerHTML);
             await browser.close();
             console.timeEnd('start');
